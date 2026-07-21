9a2f5535fce8aa626df88528a1f0c1404da6d58e
Armansyah, S.Kom, M.Pd, Gr. <40592177+cekguarman@users.noreply.github.com>
2026-07-21 10:46:10 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './berejo_gambar.tsx' // Pastikan nama file .tsx Anda disesuaikan di sini
+import App from './src/berejo_gambar.tsx' // Pastikan nama file .tsx Anda disesuaikan di sini
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
